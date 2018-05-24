8c3033261103237285d428822e31d07be7975f0d
tianhaoz95 <tianhaoz@umich.edu>
2018-05-24 10:33:25 -0700

    update

--- botXsrc/hack.py
+++ botXsrc/hack.py
@@ -1,23 +1,28 @@
 from botX.tasks import BaseTask
 from .mask_rcnn.ez import EZ
-from .peanut_arm_api.arm_component import ArmComponent
-from .peanut_camera_api.peanut_camera_api import CameraAPI
-
-config = {
-    'environment': 'robot'
-}
 
 class HackTask(BaseTask):
     def __init__(self, robot):
         super(HackTask, self).__init__(robot)
 
     def setup(self):
-        pass
+        self.detector = EZ()
 
     def run(self):
         while True:
             try:
                 object_name = input('Choose an object: ')
-
+                curr_img = self.robot.components['camera'].get_img()
+                object_dict = self.detector.detect(image_input=curr_img)
+                if object_name in object_dict:
+                    object_info = object_dict[object_name]
+                    pose = self.robot.components['grasp_planner'].plan(object_info)
+                    status = self.robot.components['arm'].move_to(pose)
+                    if status == 'success':
+                        print('done')
+                    else:
+                        print('cannot reach target')
+                else:
+                    print('Fuck, cannot find ', object_name)
             except:
                 print('Whoops')
