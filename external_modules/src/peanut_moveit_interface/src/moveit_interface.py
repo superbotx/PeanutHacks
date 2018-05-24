301cc78745dc3de5426127588f4396ea6095b7f1
achllle <achille.verheye@gmail.com>
2018-05-23 19:12:15 -0700

    block moveit during request fulfillment

--- external_modules/src/peanut_moveit_interface/src/moveit_interface.py
+++ external_modules/src/peanut_moveit_interface/src/moveit_interface.py
@@ -38,7 +38,7 @@ class MoveitInterface(object):
         """
         send the pose to moveit
         :param pose_msg: geometry_msgs/Pose
-        :return string succesa or failed (for now):
+        :return string success or failed (for now):
         """
         self.group.clear_pose_target()
         self.group.set_start_state_to_current_state()
@@ -48,7 +48,7 @@ class MoveitInterface(object):
         if found_plan:
             # block for now
             # self.group.execute(self.found_plan, wait=False)
-            self.group.execute(found_plan, wait=False)
+            self.group.execute(found_plan, wait=True)
             status = 'success'
         else:
             rospy.logerror("Couldn't find a plan to goal so not executing")
@@ -69,23 +69,10 @@ class MoveitInterface(object):
             # self.group.TERMINATE  # TODO
 
         else:  # nothing going on, execute the request
-            # xyz = np.array([req.position.x, req.position.y, req.position.z])
-            # quat = np.array([req.orientation.x, req.orientation.y, req.orientation.z, req.orientation.w])
             result = self.execute_pose(req)
 
         return MoveitInterfaceResponse(result)
 
-
-
-
-
-
-# def handler(req):
-#     if curr_srv:
-#         send_terminate_response(curr_srv)
-#     setup_group(group, req)
-#     group.go(wait=False)
-#     group.onFinish(callback=send_finish_response)
 
 if __name__ == "__main__":
     rospy.init_node('moveit_interface')
