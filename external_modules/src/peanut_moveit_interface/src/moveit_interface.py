295d025c2ecf343a01c4602569ee9aa1c5686acf
sjhansen3 <stephenjhansen8@gmail.com>
2018-05-23 20:48:41 -0700

    Changes while testing part 1

--- external_modules/src/peanut_moveit_interface/src/moveit_interface.py
+++ external_modules/src/peanut_moveit_interface/src/moveit_interface.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
 import rospy
-from util import *
 import numpy as np
 
 import moveit_commander
@@ -10,7 +9,7 @@ from geometry_msgs.msg import Pose
 from peanut_moveit_interface.srv import *
 
 
-class MoveitInterface(object):
+class MoveitInterfacer(object):
     """Interface between higher lvl code operating through botx and moveit
     requires moveit to be launched and controllers to be up
     """
@@ -38,7 +37,7 @@ class MoveitInterface(object):
                                                        queue_size=20)
 
         # TODO replace with an action service once botx can handle that
-        rospy.Service('peanut_moveit_interface', MoveitInterfaceGoal, self.higherup_handler)
+        rospy.Service('peanut_moveit_interface', MoveitInterface, self.higherup_handler)
 
     def execute_pose(self, pose_msg):
         """
@@ -46,7 +45,7 @@ class MoveitInterface(object):
         :param pose_msg: geometry_msgs/Pose
         :return string success or failed (for now):
         """
-        self.group.clear_pose_target()
+        self.group.clear_pose_targets()
         self.group.set_start_state_to_current_state()
         self.group.set_pose_target(pose_msg)
         found_plan = self.group.plan()
@@ -57,10 +56,10 @@ class MoveitInterface(object):
             self.group.execute(found_plan, wait=True)
             status = 'success'
         else:
-            rospy.logerror("Couldn't find a plan to goal so not executing")
+            rospy.logerr("Couldn't find a plan to goal so not executing")
             status = 'failed'
 
-        self.group.clear_pose_target()
+        self.group.clear_pose_targets()
 
         return status
 
@@ -75,12 +74,12 @@ class MoveitInterface(object):
             # self.group.TERMINATE  # TODO
 
         else:  # nothing going on, execute the request
-            result = self.execute_pose(req)
+            result = self.execute_pose(req.goal_pose)
 
         return MoveitInterfaceResponse(result)
 
 
 if __name__ == "__main__":
     rospy.init_node('moveit_interface')
-    moveit_interface = MoveitInterface()
+    moveit_interface = MoveitInterfacer()
     rospy.spin()
