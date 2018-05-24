c113bc7c228288fc70bd162854b01674fbb90331
sjhansen3 <stephenjhansen8@gmail.com>
2018-05-24 11:53:15 -0700

    Add dependencies

---
+++ external_modules/src/jaco_manipulation/src/grasp_planner.py
@@ -0,0 +1,280 @@
+#!/usr/bin/env python
+
+import numpy as np
+import tf
+import rospy
+import signal
+
+import spacial_location
+from spacial_location import Pose #TODO replace this with full reference, too many poses to use simply Pose
+
+from std_msgs.msg import Header
+from gqcnn.srv import GQCNNGraspPlanner
+from gqcnn.msg import GQCNNGrasp, BoundingBox
+import geometry_msgs.msg
+import std_msgs.msg
+from sensor_msgs.msg import Image, CameraInfo
+
+from perception import CameraIntrinsics, ColorImage, DepthImage
+from perception import RgbdDetectorFactory, RgbdSensorFactory
+
+from autolab_core import YamlConfig, RigidTransform
+from autolab_core import Box
+# import autolab_core.rigid_transformations.RigidTransform as RigidTransform
+
+from cv_bridge import CvBridge, CvBridgeError
+
+from gqcnn import Visualizer as vis
+
+import cv2
+import matplotlib.pyplot as plt
+import copy
+
+import matplotlib.pyplot as pyplot
+
+
+class GraspPlanner:
+    """ Abstract clss for grasp planning.
+    """
+    def __init__(self):
+        self.listener = tf.TransformListener()
+    
+    def get_grasp_plan(self, object_name):
+        assert False
+
+class GQCNNPlanner(GraspPlanner):
+    """ pass an image to the GQCNN
+    """
+    def __init__(self, camera_intrinsics, config):
+        # wait for Grasp Planning Service and create Service Proxy
+        rospy.loginfo("Waiting for GQCNN to spin up")
+        rospy.wait_for_service('plan_gqcnn_grasp')
+        self.plan_grasp = rospy.ServiceProxy('plan_gqcnn_grasp', GQCNNGraspPlanner)
+        rospy.loginfo("GQCNN service Initialized")
+
+        self.config = config
+        # get camera intrinsics
+        self.camera_intrinsics = camera_intrinsics
+        self.listener = tf.TransformListener()
+
+    def _bbox_to_msg(self, bbox):
+        """
+        Params
+        ---
+        bbox: numpy array [minX, minY, maxX, maxY] in pixels around the image 
+        Returns
+        ---
+        a bondingBox message type
+        """       
+        boundingBox = BoundingBox()
+        boundingBox.minX = bbox[0]
+        boundingBox.minY = bbox[1]
+        boundingBox.maxX = bbox[2]
+        boundingBox.maxY = bbox[3]
+        return boundingBox
+
+    def _get_new_pose(self, pose_stamped, target_frame="/world"):
+        """ Transform a pose from its frame to the desired frame
+        Params
+        ----
+        pose_stamped: A ros stamped pose with its reference frame specified
+        target_frame: The desired frame of the robot
+        Returns
+        ----
+        pose_world_stamped: The pose in the world frame
+        """
+        self.listener.waitForTransform(target_frame, self.camera_intrinsics.frame, rospy.Time(0), rospy.Duration(4.0))
+        try:
+            pose_world_stamped = self.listener.transformPose(target_frame, pose_stamped)
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.logerr("tf was not able to find a transformation between the world frame and the grasp point")
+            return False
+
+        return pose_world_stamped
+
+    def _get_transformation(self, from_frame, to_frame="world"):
+        target_frame = to_frame
+        source_frame = from_frame
+        self.listener.waitForTransform(target_frame, source_frame, rospy.Time(0), rospy.Duration(4.0))
+        try:
+            pos, quat_out = self.listener.lookupTransform(target_frame, source_frame, rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.logerr("tf was not able to find a transformation from {} to {}".format(from_frame, to_frame))
+            return False
+        #import pdb; pdb.set_trace()
+        quat = [quat_out[3], quat_out[0], quat_out[1], quat_out[2]] #the quaternion of ros has a different representation than other sys
+        return RigidTransform(quat, pos, from_frame, to_frame)
+        
+    def get_grasp_plan(self, bounding_box, color_image, depth_image):
+        """ finds the highest quality score grasp associated with the object
+
+        """
+        #grab frames for depth image and color image
+        rospy.loginfo("grabbing frames")
+
+        boundingBox = self._bbox_to_msg(bounding_box)
+        
+        pyplot.figure()
+        pyplot.subplot(2,2,1)
+        pyplot.title("color image")
+        pyplot.imshow(color_image.data)
+
+        pyplot.subplot(2,2,2)
+        pyplot.title("depth image")
+        pyplot.imshow(depth_image.data)
+
+        # inpaint to remove holes
+        inpainted_color_image = color_image.inpaint(self.config["inpaint_rescale_factor"]) #TODO make rescale factor in config
+        inpainted_depth_image = depth_image.inpaint(self.config["inpaint_rescale_factor"])
+
+        pyplot.subplot(2,2,3)
+        pyplot.title("inpaint color")
+        pyplot.imshow(inpainted_color_image.data)
+
+        pyplot.subplot(2,2,4)
+        pyplot.title("inpaint depth")
+        pyplot.imshow(inpainted_depth_image.data)
+        pyplot.show()
+        
+        try:
+            rospy.loginfo("Sending grasp plan request to gqcnn server")
+            planned_grasp_data = self.plan_grasp(inpainted_color_image.rosmsg, inpainted_depth_image.rosmsg, self.camera_intrinsics.rosmsg, boundingBox)
+
+            planned_grasp_pose_msg = planned_grasp_data.grasp.pose
+            grasp_succes_prob = planned_grasp_data.grasp.grasp_success_prob
+            grasp = planned_grasp_data.grasp
+
+            rospy.loginfo("Grasp service request response: {}".format(planned_grasp_data))
+
+        except rospy.ServiceException, e:
+            rospy.logerr("Service call failed: \n %s" % e)  
+
+        rotation_quaternion = np.asarray([grasp.pose.orientation.w, grasp.pose.orientation.x, grasp.pose.orientation.y, grasp.pose.orientation.z]) 
+        translation = np.asarray([grasp.pose.position.x, grasp.pose.position.y, grasp.pose.position.z])
+        T_grasp_camera = RigidTransform(rotation_quaternion, translation, "grasp", self.camera_intrinsics.frame)
+
+        T_world_camera = self._get_transformation(self.camera_intrinsics.frame) #TODO this should be a rigidbody.getfromtransform call
+        
+        rotation = np.array([[0, 0, 1],[0, 1, 0],[-1, 0, 0]])
+        translation = np.array([0, 0, 0])
+        T_grasp_gripper = RigidTransform(rotation, translation, from_frame="gripper", to_frame="grasp")
+        T_grasp_world = T_world_camera * T_grasp_camera * T_grasp_gripper
+
+        import pdb; pdb.set_trace()
+        t_quat = [T_grasp_world.quaternion[1], T_grasp_world.quaternion[2], T_grasp_world.quaternion[3], T_grasp_world.quaternion[0]]
+        grasp_pose_world = Pose(T_grasp_world.position, t_quat, frame="/world")
+
+        pre_grasp_pose = offset_hand(grasp_pose_world, unit_vector=[0,0,1])
+
+        return pre_grasp_pose, grasp_pose_world
+
+class ARTrackPlanner(GraspPlanner):
+    """ One small step above hard coding - uses AR trackers and a dictionary to find poses
+    #TODO implement dictionary with pose offsets based on object type
+    """
+    def __init__(self):
+        GraspPlanner.__init__(self)
+        self.object_to_marker_num = {"cup": 8, "spoon":99}
+
+    def get_grasp_plan(self, object_name, marker_num = None):
+        """ The grasp plan for the AR tracker is simply based on a fixed offset for each object
+        Params
+        ---
+        object_name: The name of the object to be grasped. e.g. cup
+        marker_num: the associated marker for the object. If you would like
+        to override the parameter put in the dictionary
+        Returns
+        ---
+        A tuple containing pregrasp pose and grasp pose
+        """
+        if object_name not in self.object_to_marker_num and not marker_num:
+            raise ValueError("no marker assigned for marker {}".format(object_name))
+        if marker_num:
+            self.object_to_marker_num[object_name] = marker_num
+        ar_frame = "/ar_marker_{}".format(self.object_to_marker_num[object_name])
+        world_frame = "/world"
+
+        self.listener.waitForTransform(ar_frame, world_frame, rospy.Time(0), rospy.Duration(4.0))
+        try:
+            (trans, rot) = self.listener.lookupTransform(world_frame, ar_frame, rospy.Time(0))
+            #break
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.logerr("Did not get for AR tracker tf frame {}".format(ar_frame))
+        hard_code_pose = Pose.load("opposite_grasp")
+        print "z: ", hard_code_pose.position.z
+        
+        
+        rospy.loginfo("AR tracker translation: {}, rotation: {}".format(trans, rot))
+        rospy.loginfo("Hardcoded pose {}".format(hard_code_pose))
+
+        cup_position = np.asarray([trans[0], trans[1], hard_code_pose.position.z])
+
+        grasp_pose = Pose(cup_position, hard_code_pose.orientation)
+        rospy.loginfo("found tracker positon {}".format(grasp_pose))
+
+        pre_grasp_pose = offset_hand(grasp_pose, offset_dist=0.1)
+        return pre_grasp_pose, grasp_pose
+
+def offset_hand(pose_input, unit_vector=[1, 0, 0] ,offset_dist=0.1):
+    """ Find the pre grasp pose by offsetting the hand backwards from its current position
+    grasp_pose: the pose of the grasp location
+    offset_dist: the amount to offset off the object
+    Returns
+    ---
+    pre_grasp_pose: the offsetted pose of the object
+    """
+    #use the unit z vector because thats the direction out of the hand
+    quat = pose_input.orientation[0:4]
+
+    quat_inv = tf.transformations.quaternion_inverse(quat)
+    direction = spacial_location.qv_mult(quat, unit_vector)
+    pre_grasp_position = pose_input.position - direction*offset_dist
+
+    pre_grasp_pose = Pose(pre_grasp_position, pose_input.orientation)
+
+    pose_input.show_position_marker(ident = 1, label = "grasp pose")
+    pre_grasp_pose.show_position_marker(ident = 2, label = "pregrasp pose")
+    # import pdb; pdb.set_trace()
+
+    return pre_grasp_pose
+
+def test_offset_hand():
+    rospy.sleep(5)
+    quat = tf.transformations.random_quaternion()
+    print quat
+    grasp_pose = Pose([0.5,0.5,0.5], quat)
+    offset_hand(grasp_pose)
+
+def test_GQCNN():
+
+    config = YamlConfig('/home/baymax/catkin_ws/src/jaco_manipulation/cfg/grasp_test.yaml')
+
+    # create rgbd sensor
+    rospy.loginfo('Creating RGBD Sensor')
+    sensor_cfg = config['sensor_cfg']
+    sensor_type = sensor_cfg['type']
+    sensor = RgbdSensorFactory.sensor(sensor_type, sensor_cfg)
+    sensor.start()
+    rospy.loginfo('Sensor Running')
+
+    # setup safe termination
+    def handler(signum, frame):
+        rospy.loginfo('caught CTRL+C, exiting...')        
+        if sensor is not None:
+            sensor.stop()
+        if robot is not None:
+            robot.stop()
+        if subscriber is not None and subscriber._started:
+            subscriber.stop()            
+        exit(0)
+    signal.signal(signal.SIGINT, handler)
+
+    planner = GQCNNPlanner(sensor, config)
+    # rospy.sleep(10)
+    planner.get_grasp_plan("cup")
+
+if __name__ == '__main__':
+    rospy.init_node("Grasp_planner_test_node", log_level=rospy.DEBUG)
+    test_offset_hand()
+    
+    #rospy.spin()
---
+++ external_modules/src/jaco_manipulation/src/moveit_interface.py
@@ -0,0 +1,377 @@
+#!/usr/bin/env python
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+from moveit_msgs.srv import GetPositionIK, GetPositionFK
+
+import geometry_msgs.msg
+import shape_msgs.msg
+import std_msgs.msg
+
+import rospkg
+import kinova_msgs.msg
+import actionlib
+from spacial_location import Pose
+import numpy as np
+from grasp_planner import ARTrackPlanner
+
+class RobotPlanner:
+    #TODO consider renaming this - it has expanded beyond a planner
+    def __init__(self):
+        moveit_commander.roscpp_initialize(sys.argv)
+
+        self.robot = moveit_commander.RobotCommander()
+        self.scene = moveit_commander.PlanningSceneInterface()
+        self.group = moveit_commander.MoveGroupCommander("arm")
+
+        self.group.set_planner_id("RRTConnectkConfigDefault") 
+        rospy.sleep(1)
+        self.group.set_goal_tolerance(0.001)
+
+        self.group.allow_replanning(True)
+        
+
+        self.add_scene_items()
+        self.solved_plan = None
+        self.eef_name = "j2s7s300_end_effector"
+
+        # Set up the FK service
+        rospy.wait_for_service('/compute_fk')
+        self.compute_fk = rospy.ServiceProxy('/compute_fk', GetPositionFK)
+
+
+        # Set up the IK service
+        rospy.wait_for_service('/compute_ik')
+        self.compute_ik = rospy.ServiceProxy('/compute_ik', GetPositionIK)
+
+    def add_scene_items(self):
+        """
+        Add obstacles into the scene for planning with moveit
+        """
+        #TODO add scene item for tripod and bookshelf
+        self.scene.remove_world_object("ground")
+        #self.scene.remove_world_object("table")
+
+        rospy.sleep(1)
+
+        p = geometry_msgs.msg.PoseStamped()
+        p.header.frame_id = "world"
+        p.pose.position.x = -1
+        p.pose.position.y = 0.5
+        p.pose.position.z = -0.11
+        p.pose.orientation.x = 0
+        p.pose.orientation.y = 0
+        p.pose.orientation.z = 0
+        p.pose.orientation.w = 1
+        print("add box")
+        
+        #add the desk
+        #TODO get orientation and size correct
+        rospack = rospkg.RosPack()
+        package_path = rospack.get_path('jaco_manipulation')
+        model_folder_path = package_path + "/models/"
+
+        #self.scene.add_mesh("table",p,model_folder_path+"Desk.stl",size = (0.01, 0.01, 0.01))
+
+        p.pose.position.y = 0
+        p.pose.position.x = 0
+        #TODO add ros param to turn off ground
+        self.scene.add_box("ground", p, (3, 3, 0.02)) #add a "ground plane"
+
+
+        p.pose.position.z = -0.05
+        self.scene.add_box("base",p,(0.15,0.15,0.15)) #add a base for safety
+
+    def plan(self, pose_input):
+        """ find a plan to the position
+        Params
+        ---
+        pose_input: The pose to plan to. `string` or `Pose`
+        Returns
+        ---
+        1 if plan succeeded 0 otherwise
+        """
+        if isinstance(pose_input, basestring):
+            pose = Pose.load(pose_input)
+        elif isinstance(pose_input, Pose):
+            pose = pose_input
+        else:
+            raise ValueError("plan only supports pose objects")
+
+        self.group.clear_pose_targets()
+        
+        self.group.set_pose_target(pose.ros_message)
+        rospy.loginfo("Planning to {}".format(pose))
+        self.group.set_start_state_to_current_state()
+        rospy.sleep(1)
+        self.solved_plan = self.group.plan() #automatically displays trajectory
+        if self.solved_plan is None:
+            rospy.logerr("Plan not solved")
+            return None
+
+        cur_joints = list(self.solved_plan.joint_trajectory.points[-1].positions)
+        posefk = self.fk(cur_joints).pose_stamped[0].pose
+        final_pose_res = Pose([posefk.position.x, posefk.position.y, posefk.position.z],[posefk.orientation.x, posefk.orientation.y, posefk.orientation.z, posefk.orientation.w])
+        return pose, final_pose_res 
+        #TODO add return value for success or failure
+    
+    def fk(self, joints, links=['j2s7s300_end_effector']):
+        """ Computes the forward kinematics 
+        joints: an array of joint positions
+        """
+        header = std_msgs.msg.Header()
+        robot_state = moveit_msgs.msg.RobotState()
+        robot_state.joint_state.name = ['j2s7s300_joint_{}'.format(i) for i in range(1,8)]
+        robot_state.joint_state.position = joints
+        return self.compute_fk(header, links, robot_state)
+
+    
+    # def ik(self, pose_stamped, group_name='arm'):
+    #     """ Computes the inverse kinematics """
+    #     req = PositionIKRequest()
+    #     req.group_name = group_name
+    #     req.pose_stamped = pose_stamped
+    #     req.timeout.secs = 0.1
+    #     req.avoid_collisions = False
+
+    #     try:
+    #         res = self.compute_ik(req)
+    #         return res
+    #     except rospy.ServiceException, e:
+    #         print("IK service call failed: {}".format(e))
+
+    #TODO add waypoint planning for pregrasp -> grasp
+    def plan_waypoints(self, waypoints):
+        """
+        No more than jump_threshold is allowed as change in distance 
+        in the configuration space of the robot (this is to prevent 'jumps' in IK solutions).
+        """
+        #TODO check waypoints input
+
+        jump_threshold = 0.0
+        eef_step = 0.01
+        (self.solved_plan, percent_solved) = self.group.compute_cartesian_path(waypoints, eef_step, jump_threshold, avoid_collisions = True)
+        rospy.loginfo("{} percent of path planned as described by the waypoints".format(percent_solved))
+
+    def execute(self):
+        """ Execute a planned path
+        """
+        if self.solved_plan is None:
+            rospy.logwarn("Plan is None, planning might not have been completed or failed")
+            return
+        self.group.execute(self.solved_plan)
+        self.solved_plan = None #TODO is it appropriate to set solved_plan to none after its been executed
+        #Do we want to solve the same plan twice in a row? I think no
+        print("execution complete")
+    
+    @property
+    def current_pose(self):
+        cur_pose = self.group.get_current_pose(self.eef_name)
+        position = [cur_pose.pose.position.x, cur_pose.pose.position.y, cur_pose.pose.position.z]
+        orientation = [cur_pose.pose.orientation.x, cur_pose.pose.orientation.y, cur_pose.pose.orientation.z, cur_pose.pose.orientation.w]
+        pose = Pose(position,orientation)
+        return pose
+
+
+class GripController:
+    """ A wrapper to send commands to the robot hand.
+    The kinova hand uses an action server for grip actions.
+    """
+    def __init__(self):
+        self.finger_maxDist = 18.9/2/1000  # max distance for one finger
+        self.finger_maxTurn = 6800  # max thread rotation for one finger
+        
+        action_address = '/j2s7s300_driver/fingers_action/finger_positions'
+
+        self.grip_action_client = actionlib.SimpleActionClient(action_address,
+                                            kinova_msgs.msg.SetFingersPositionAction)
+
+    def grip(self, units, finger_positions):
+        """ 
+        Params
+        ---
+        units: a string representing units ()
+        finger_values: an array like object of dim 3
+        Returns
+        ---
+        None if the grip timed out
+        """
+        turn, meter, percent = self._convert_units(units, finger_positions)
+        positions_temp1 = [max(0.0, n) for n in turn]
+        positions_temp2 = [min(n, self.finger_maxTurn) for n in positions_temp1]
+        positions = [float(n) for n in positions_temp2]
+        return self._execute_grip(turn)
+
+    def _execute_grip(self, finger_positions):
+        """ move the gripper to the finger position
+        """
+        if len(finger_positions) != 3:
+            raise ValueError("wrong number of finger specified")
+        
+        print("wating for action client")
+        self.grip_action_client.wait_for_server()
+
+        goal = kinova_msgs.msg.SetFingersPositionGoal()
+        goal.fingers.finger1 = float(finger_positions[0])
+        goal.fingers.finger2 = float(finger_positions[1])
+        goal.fingers.finger3 = float(finger_positions[2])
+        print("sending grip goal {}".format(goal))
+        self.grip_action_client.send_goal(goal)
+        if self.grip_action_client.wait_for_result(rospy.Duration(5.0)):
+            return self.grip_action_client.get_result()
+        else:
+            self.grip_action_client.cancel_all_goals()
+            rospy.logwarn('        the gripper action timed-out')
+            return None
+
+    def _convert_units(self, unit_, finger_value_):
+        """
+        converts units from the input unit (unit_) to all units (turn_, meter_, percent_)
+        ---
+        unit_: a string representing the input units (turn, mm, percent)
+        finger_value_: a  value associated with the grasp
+        Returns
+        ---
+        a tuple containing all units turn units, finger meter, finger percent
+        """
+        # transform between units
+        if unit_ == 'turn':
+            # get absolute value
+            finger_turn_ = finger_value_
+            finger_meter_ = [x * self.finger_maxDist / self.finger_maxTurn for x in finger_turn_]
+            finger_percent_ = [x / self.finger_maxTurn * 100.0 for x in finger_turn_]
+
+        elif unit_ == 'mm':
+            # get absolute value
+            finger_turn_ = [x/1000 * self.finger_maxTurn / self.finger_maxDist for x in finger_value_]
+            finger_meter_ = [x * self.finger_maxDist / self.finger_maxTurn for x in finger_turn_]
+            finger_percent_ = [x / self.finger_maxTurn * 100.0 for x in finger_turn_]
+        
+        elif unit_ == 'percent':
+            # get absolute value
+            finger_turn_ = [x/100.0 * self.finger_maxTurn for x in finger_value_]
+            finger_meter_ = [x * self.finger_maxDist / self.finger_maxTurn for x in finger_turn_]
+            finger_percent_ = [x / self.finger_maxTurn * 100.0 for x in finger_turn_]
+        else:
+            raise Exception("Finger value have to be in turn, mm or percent")
+
+        return finger_turn_, finger_meter_, finger_percent_
+
+def dynamic_pick_place():
+    """ A somewhat ugly function which runs pick and place using the AR trackers
+    """
+    rospy.sleep(2)
+    robot_planner = RobotPlanner()
+    grip_controller = GripController()
+
+    #get the ARTracker Pose
+    grasp_planner = GQCNNPlanner()
+    pregrasp_pose, grasp_pose = grasp_planner.get_grasp_plan("cup")
+
+    #home grip location
+    robot_planner.plan("home_grip")
+    grip_controller.grip("percent",[0,0,0])
+    #raw_input("plan to home_grip commplete, anykey and enter to execute")
+    robot_planner.execute()
+
+    #pre grip location
+    robot_planner.plan(pregrasp_pose)
+    raw_input("plan to grasp_pre_hardcode commplete, anykey and enter to execute")
+    robot_planner.execute()
+
+    #grip location
+    robot_planner.plan(grasp_pose)
+    raw_input("plan to grasp_hardcode commplete, anykey and enter to execute")
+    robot_planner.execute()
+    
+    #grip object
+    #raw_input("grip object? anykey to execute")
+    grip_controller.grip("percent",[75,75,75])
+    
+    #target pre
+    robot_planner.plan("grasp_target_pre")
+    #raw_input("plan to grasp_target_pre commplete, anykey and enter to execute")
+    robot_planner.execute()
+
+    #target location
+    robot_planner.plan("grasp_target")
+    #raw_input("plan to grasp_target commplete, anykey and enter to execute")
+    robot_planner.execute()
+
+    #release object
+    #raw_input("release object? anykey to execute")
+    grip_controller.grip("percent",[0,0,0])
+    rospy.sleep(1)
+    #go bak to pre grip location
+    #robot_planner.plan("grasp_pre_hardcode")
+    #raw_input("plan to back to grasp_pre_hardcode commplete, anykey and enter to execute")
+    #robot_planner.execute()
+
+    #home grip location
+    robot_planner.plan("home_grip")
+    #raw_input("plan to home_grip commplete, anykey and enter to execute")
+    robot_planner.execute()
+
+def test_plan_waypoints():
+    rospy.sleep(2)
+    robot_planner = RobotPlanner()
+    grip_controller = GripController()
+    
+    downstream_error = []
+    rrt_error = []
+    for i in range(5):
+        #home grip location
+        try:
+            downstream, rrt = move_to_position("pos_1", robot_planner)
+            downstream_error.append(downstream)
+            rrt_error.append(rrt)
+
+            #pre grip location
+            downstream, rrt = move_to_position("pos_2", robot_planner)
+            downstream_error.append(downstream)
+            rrt_error.append(rrt)
+        except rospy.ROSInterruptException, e:
+            print e
+            break
+    print "\n********RRT Error*********\n", rrt_error
+    print "\n*******DOWNSTREAM*********\n", downstream_error
+
+def move_to_position(position_name, robot_planner):
+    #home grip location
+    pose_request, pose_final_plan = robot_planner.plan(position_name)
+
+    raw_data = raw_input("plan to {} commplete, anykey and enter to execute, q to exit".format(position_name))
+    if raw_data == 'q':
+        raise ValueError("shutdown requested mid test")
+        return
+
+    robot_planner.execute()
+    rospy.sleep(2)
+    
+    act_pose = robot_planner.current_pose
+    downstream_error = act_pose.position - pose_final_plan.position
+    rrt_error = pose_request.position - pose_final_plan.position
+    
+    
+    print "actual pose: ", act_pose
+    print "requested pose:  ", pose_request
+    print "final pose of plan:  ", pose_final_plan
+    
+    print "rrt_error: ", rrt_error
+    rrt_norm = np.linalg.norm(np.asarray(rrt_error[0:3]))
+    print "rrt_norm:   ", rrt_norm
+
+    downstream_norm = np.linalg.norm(np.asarray(downstream_error[0:3]))
+    print "downstream_error: ", downstream_error
+    print "downstream_norm: ", downstream_norm 
+    return downstream_norm, rrt_norm
+
+if __name__ == '__main__':
+    rospy.init_node('moveit_interface',
+                        anonymous=True)
+
+    dynamic_pick_place()
+    #test_plan_waypoints()
