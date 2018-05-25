a7ee836aa5f47ec3a5519e1f717318a5d4c7f3b4
achllle <achille.verheye@gmail.com>
2018-05-25 11:21:45 -0700

    Integrate interface with grasping execution, tested working

--- external_modules/src/peanut_demo_hack/src/grasp_pipeline.py
+++ external_modules/src/peanut_demo_hack/src/grasp_pipeline.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 
-import sys
-import copy
 import rospy
-import moveit_commander
-import moveit_msgs.msg
-import geometry_msgs.msg
+from geometry_msgs.msg import Pose
+from std_msgs.msg import String, Float64
 import numpy as np
 
-from peanut_moveit_interface.srv import *
+from peanut_moveit_interface.srv import MoveitInterface, GripperInterface
 
 def main():
     #### 0 - setup
-    rospy.wait_for_service('')
+    rospy.loginfo('waiting for services to come up...')
+    rospy.wait_for_service('peanut_moveit_interface')
+    rospy.wait_for_service('peanut_gripper_interface')
+    execute_arm_move = rospy.ServiceProxy('peanut_moveit_interface', MoveitInterface)
+    execute_gripper_move = rospy.ServiceProxy('peanut_gripper_interface', GripperInterface)
+    rospy.loginfo('services reported!')
 
     #### 1 - get string input from user denoting an object
-    object_to_pick = raw_input()
-
+    object_to_pick = raw_input('please enter which object youd like to pick: ')
+    rospy.loginfo('selected {} for picking'.format(object_to_pick))
 
     #### 2 - find bounding box for class
     # result is an array with 4 numbers, plus two images
@@ -24,23 +26,29 @@ def main():
     #### 3 - find grasp within bounding box
 
     #### 4 - execute grasp
+    pose_msg = Pose()
+
+    pose_msg.position.x = .2
+    pose_msg.position.y = -.270
+    pose_msg.position.z = .48
+    pose_msg.orientation.x = 0.67
+    pose_msg.orientation.y = -0.15
+    pose_msg.orientation.z = -0.69
+    pose_msg.orientation.w = 0.17
+
+    unit_msg = String()
+    unit_msg.data = 'percent'
+    finger_position_msg = [Float64(50), Float64(50), Float64(50)]
+
     try:
-        add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
-        resp1 = add_two_ints(x, y)
-        return resp1.sum
+        resp1 = execute_arm_move(pose_msg)
+    except rospy.ServiceException, e:
+        print "Service call failed: %s" % e
+    try:
+        resp2 = execute_gripper_move(unit_msg, finger_position_msg)
     except rospy.ServiceException, e:
         print "Service call failed: %s" % e
 
-# wpose = geometry_msgs.msg.Pose()
-# orientation = np.array([1, 0, 1, 0])
-# orientation = orientation / np.linalg.norm(orientation)  # enforce unit quaternion
-# wpose.orientation.x = orientation[0]
-# wpose.orientation.y = orientation[1]
-# wpose.orientation.z = orientation[2]
-# wpose.orientation.w = orientation[3]
-# wpose.position.x = 0.4
-# wpose.position.y = 0.0
-# wpose.position.z = 0.1
 
 if __name__ == '__main__':
     rospy.init_node('hacking_now')
